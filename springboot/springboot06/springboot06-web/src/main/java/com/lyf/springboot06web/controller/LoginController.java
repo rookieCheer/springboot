e2118ace05a4ecf02075fbb1ca4f4dd589ac6c71
lyf <a1b2c3d4>
2018-12-18 21:58:20 +0800

    验证码 两套随机

--- springboot/springboot06/springboot06-web/src/main/java/com/lyf/springboot06web/controller/LoginController.java
+++ springboot/springboot06/springboot06-web/src/main/java/com/lyf/springboot06web/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.lyf.springboot06web.controller;
 
+import com.lyf.springboot06service.util.VerifyCode;
+import com.lyf.springboot06service.util.VerifyUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
@@ -7,8 +9,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+import java.util.Random;
 
 @Controller
 public class LoginController {
@@ -39,33 +50,42 @@ public class LoginController {
 
     /**
      * post登陆
+     *
      * @param username
      * @param password
      * @param model
      * @return
      */
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(String username, String password, Model model){
+    @RequestMapping(value = "/login")
+    public String login(String username, String password, String verify, Model model, HttpServletRequest request) {
         //添加用户认证信息
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username,password);
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, password);
+        HttpSession session = request.getSession();
         //进行验证，这里可以捕获异常，然后返回对应信息
         try {
-            subject.login(usernamePasswordToken);
-            return "/l/index";
-        }catch (IncorrectCredentialsException ice){
-            model.addAttribute("message","密码错误");
-        }catch (UnknownAccountException uae) {
-            model.addAttribute("message",uae.getMessage());
-        }catch (LockedAccountException lae) {
-            model.addAttribute("message",lae.getMessage());
-        }catch (ExcessiveAttemptsException eae) {
-            model.addAttribute("message","次数超出");
+            String imageCode = session.getAttribute("imageCode") + "";
+            if (verify != null && verify.equalsIgnoreCase(imageCode)) {
+                subject.login(usernamePasswordToken);
+                return "/l/index";
+            } else {
+                model.addAttribute("message", "验证码错误!");
+            }
+        } catch (IncorrectCredentialsException ice) {
+            model.addAttribute("message", "密码错误");
+        } catch (UnknownAccountException uae) {
+            model.addAttribute("message", uae.getMessage());
+        } catch (LockedAccountException lae) {
+            model.addAttribute("message", lae.getMessage());
+        } catch (ExcessiveAttemptsException eae) {
+            model.addAttribute("message", "次数超出");
         }
         return "/l/login";
     }
+
     /**
      * 退出
+     *
      * @return
      */
     @RequestMapping("/logout")
@@ -80,12 +100,68 @@ public class LoginController {
 
     /**
      * 拦截器 没有登录时  跳转页面  不然404
+     *
      * @return
      */
     @RequestMapping("/loginForm")
     public String loginForm() {
         return "/l/login";
     }
+
+    /**
+     * 两套 生成验证码  随机
+     *
+     * @param response
+     * @param request
+     * @throws Exception
+     */
+    @GetMapping("/skip/getcode")
+    public void getCode(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        HttpSession session = request.getSession();
+        Random random = new Random();
+        int i = random.nextInt(2) + 1;
+        switch (i) {
+//            带背景
+            case 1:
+                //利用图片工具生成图片
+                //第一个参数是生成的验证码，第二个参数是生成的图片
+                Object[] objs = VerifyUtil.createImage();
+                //将验证码存入Session
+                session.setAttribute("imageCode", objs[0]);
+                //将图片输出给浏览器
+                BufferedImage image = (BufferedImage) objs[1];
+                response.setContentType("image/png");
+                OutputStream os = response.getOutputStream();
+                ImageIO.write(image, "png", os);
+                break;
+            //不带背景
+            case 2:
+                //创建对象
+                VerifyCode vc = new VerifyCode();
+                //获取图片对象
+                BufferedImage bi = vc.getImage();
+                //获得图片的文本内容
+                String text = vc.getText();
+                // 将系统生成的文本内容保存到session中
+                session.setAttribute("imageCode", text);
+                //向浏览器输出图片
+                VerifyCode.output(bi, response.getOutputStream());
+                break;
+            default:
+                //创建对象
+                VerifyCode vcd = new VerifyCode();
+                //获取图片对象
+                BufferedImage bid = vcd.getImage();
+                //获得图片的文本内容
+                String imageCode = vcd.getText();
+                // 将系统生成的文本内容保存到session中
+                session.setAttribute("imageCode", imageCode);
+                //向浏览器输出图片
+                VerifyCode.output(bid, response.getOutputStream());
+                break;
+        }
+    }
+
 
 //    /**
 //     * 没有权限页面跳转
